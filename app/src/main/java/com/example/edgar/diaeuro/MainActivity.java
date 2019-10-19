eb20c84f189323d09328ba034cf17d7946a05cc2
Ex70 <edgaarhdez96@gmail.com>
2019-10-19 09:33:31 -0500

    Autenticación VF
    *Login dinámico

--- app/src/main/java/com/example/edgar/diaeuro/MainActivity.java
+++ app/src/main/java/com/example/edgar/diaeuro/MainActivity.java
@@ -2,148 +2,37 @@ package com.example.edgar.diaeuro;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.squareup.picasso.Picasso;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_REQUEST_CODE = 7117; //Puede ser cualquiera
-    public final int GOOGLE_SIGN = 123;
-    FirebaseAuth mAuth;
-    Button btn_login, btn_logout;
     List<AuthUI.IdpConfig> providers;
     Button btn_sign_out;
-    TextView text;
-    ImageView image;
-    ProgressBar progressBar;
-    GoogleSignInClient mGoogleSingInClient;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_login = findViewById(R.id.login);
-        btn_logout = findViewById(R.id.logout);
-        text = findViewById(R.id.text);
-        image = findViewById(R.id.image);
-        progressBar = findViewById(R.id.progress_circular);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
-                .Builder()
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSingInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-
-        btn_login.setOnClickListener(v -> SignInGoogle());
-        btn_logout.setOnClickListener(v -> Logout());
-
-        if (mAuth.getCurrentUser() != null) {
-            FirebaseUser user = mAuth.getCurrentUser();
-            updateUI(user);
-        }
-    }
-
-    void SignInGoogle() {
-        progressBar.setVisibility(View.VISIBLE);
-        Intent signIntent = mGoogleSingInClient.getSignInIntent();
-        startActivityForResult(signIntent, GOOGLE_SIGN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == GOOGLE_SIGN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn
-                    .getSignedInAccountFromIntent(data);
-
-            try{
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                if (account != null) firebaseAuthWithGoogle(account);
-            }catch (ApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        Log.d("TAG","firebaseAuthWithGoogle: " + account.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this,task -> {
-                    if (task.isSuccessful()){
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Log.d("TAG", "signin success");
-
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        updateUI(user);
-                    }else{
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Log.w("TAG", "signin failure", task.getException());
-
-                        Toast.makeText(this,"SignIn Failed!",Toast.LENGTH_SHORT);
-                        updateUI(null);
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-
-        if(user != null){
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String photo = String.valueOf(user.getPhotoUrl());
-
-            text.append("info : \n");
-            text.append(name + "\n");
-            text.append(email);
-
-            Picasso.get().load(photo).into(image);
-            btn_login.setVisibility(View.INVISIBLE);
-            btn_logout.setVisibility(View.VISIBLE);
-        }else{
-            text.setText(getString(R.string.firebase_login));
-            Picasso.get().load(R.drawable.ic_launcher_foreground).into(image);
-            btn_login.setVisibility(View.VISIBLE);
-            btn_logout.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    void Logout(){
-        FirebaseAuth.getInstance().signOut();
-        mGoogleSingInClient.signOut().addOnCompleteListener(this,task -> updateUI(null));
-    }
-
-    /*btn_sign_out =(Button)findViewById(R.id.btn_sign_out);
+        btn_sign_out =(Button)findViewById(R.id.btn_sign_out);
         btn_sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,15 +52,9 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        });-*/
+        });
 
-
-
-
-
-
-
-        /*Inicio de provider
+        //Inicio de provider
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(), //Email Builder
                 new AuthUI.IdpConfig.PhoneBuilder().build(), //Phone Builder
@@ -180,18 +63,20 @@ public class MainActivity extends AppCompatActivity {
         );
 
         showSignInOptions();
-    }*/
+    }
 
     private void showSignInOptions() {
         startActivityForResult(
                 AuthUI.getInstance().createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setTheme(R.style.MyTheme)
-                .build(),MY_REQUEST_CODE
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                        .setLogo(R.drawable.escudo)
+                        .setTosAndPrivacyPolicyUrls("", "http://ueh.edu.mx/privacidad3")
+                        .build(),MY_REQUEST_CODE
         );
     }
 
-    /*@Override
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == MY_REQUEST_CODE){
@@ -209,5 +94,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    */
 }
